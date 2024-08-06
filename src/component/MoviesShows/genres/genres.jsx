bb21fcbb2b76389b2bc862f9edbe221b556fa313
Jaloliddin <jaloliddintuitdif31@gmail.com>
2024-08-06 23:19:24 +0500

    ok

--- src/component/MoviesShows/genres/genres.jsx
+++ src/component/MoviesShows/genres/genres.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import "./genres.css"
-const Genres = ({item}) => {
-    
-  return (
-    <div className='p4 genres'>Genres</div>
-  )
+const Genres = ({ item }) => {
+
+    return (
+        <div className='p4 genres-box'>
+
+            <div className="genres">
+                {item.name}
+            </div>
+        </div>
+    )
 }
 
 export default Genres
